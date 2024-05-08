baf8d1323bb3182486bc3631507ea0d63656373b
Cuga77 <bogatov2021@mail.ru>
2024-05-08 11:41:13 +0300

    BAD_diffusion_realization

--- task_1/bruesselator.cpp
+++ task_1/bruesselator.cpp
@@ -7,27 +7,53 @@
 // dx/dt = x^2y/2 + a - bx -x
 // dy/dt = -x^2y/2 + bx
 
+// with diffusion
+// dx/dt = x^2y/2 + a - bx -x + D(d^2x/dx^2 + d^2x/dy^2)
+// dy/dt = -x^2y/2 + bx + D(d^2y/dx^2 + d^2y/dy^2)
 
-constexpr int WINDOW_WIDTH = 200;
-constexpr int WINDOW_HEIGHT = 200;
+
+constexpr int WINDOW_WIDTH = 400;
+constexpr int WINDOW_HEIGHT = 400;
 
 constexpr double A = 1.0;
-constexpr double B = 3.0;
+constexpr double B = 3.5;
 constexpr double B1 = (B + 1);
 
-constexpr double Dd = 0.0075;
-constexpr double DT = 0.0013;
-constexpr double DX = 0.015;
-constexpr double DY = 0.028;
+constexpr double Dd = (double)(1.1E-4);
+constexpr double DT = 0.0001;
+constexpr double DX = 0.0015;
+constexpr double DY = 0.0028;
 
 // compile: g++ -O3 bruesselator.cpp -lsfml-graphics -lsfml-window -lsfml-system -fopenmp && ./a.out
 
-double X(double x, double y) {
-    return A - B1*x + x*x*y;
+double X(double x, double y, double dx2, double dy2) {
+    return A - B1*x + x*x*y + Dd * (dx2 + dy2);
 }
 
-double Y(double x, double y) {
-    return B * x - x*x*y;
+double Y(double x, double y, double dx2, double dy2) {
+    return B * x - x*x*y + Dd * (dx2 + dy2);
+}
+
+//диффузия ПОСЛЕ
+
+void diffusion(double** x, double** y, double** new_x, double** new_y, double D, double dt, double dx, double dy) {
+    #pragma omp parallel for
+    for (int i = 0; i < WINDOW_WIDTH; i++) {
+        for (int j = 0; j < WINDOW_HEIGHT; j++) {
+            int i_minus = (i > 0) ? i - 1 : WINDOW_WIDTH - 1;
+            int i_plus = (i < WINDOW_WIDTH - 1) ? i + 1 : 0;
+            int j_minus = (j > 0) ? j - 1 : WINDOW_HEIGHT - 1;
+            int j_plus = (j < WINDOW_HEIGHT - 1) ? j + 1 : 0;
+
+            double dx2_x = (x[i_minus][j] - 2 * x[i][j] + x[i_plus][j]) / (dx * dx);
+            double dy2_x = (x[i][j_minus] - 2 * x[i][j] + x[i][j_plus]) / (dy * dy);
+            new_x[i][j] = x[i][j] + dt * X(x[i][j], y[i][j], dx2_x, dy2_x);
+
+            double dx2_y = (y[i_minus][j] - 2 * y[i][j] + y[i_plus][j]) / (dx * dx);
+            double dy2_y = (y[i][j_minus] - 2 * y[i][j] + y[i][j_plus]) / (dy * dy);
+            new_y[i][j] = y[i][j] + dt * Y(x[i][j], y[i][j], dx2_y, dy2_y);
+        }
+    }
 }
 
 void rk4 (double** x, double** y, double** vx, double** vy, double dt) {
@@ -69,40 +95,40 @@ void rk4 (double** x, double** y, double** vx, double** vy, double dt) {
     #pragma omp parallel for
     for (int i = 0; i < WINDOW_WIDTH; i++) {
         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-            k1x[i][j] = dt * X(x[i][j], y[i][j]);
-            k1y[i][j] = dt * Y(x[i][j], y[i][j]);
-            k1vx[i][j] = dt * X(vx[i][j], vy[i][j]);
-            k1vy[i][j] = dt * Y(vx[i][j], vy[i][j]);
+            k1x[i][j] = dt * X(x[i][j], y[i][j], DX, DY);
+            k1y[i][j] = dt * Y(x[i][j], y[i][j], DX, DY);
+            k1vx[i][j] = dt * X(vx[i][j], vy[i][j], DX, DY);
+            k1vy[i][j] = dt * Y(vx[i][j], vy[i][j], DX, DY);
         }
     }
 
     #pragma omp parallel for
     for (int i = 0; i < WINDOW_WIDTH; i++) {
         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-            k2x[i][j] = dt * X(x[i][j] + k1x[i][j] / 2, y[i][j] + k1y[i][j] / 2);
-            k2y[i][j] = dt * Y(x[i][j] + k1x[i][j] / 2, y[i][j] + k1y[i][j] / 2);
-            k2vx[i][j] = dt * X(vx[i][j] + k1vx[i][j] / 2, vy[i][j] + k1vy[i][j] / 2);
-            k2vy[i][j] = dt * Y(vx[i][j] + k1vx[i][j] / 2, vy[i][j] + k1vy[i][j] / 2);
+            k2x[i][j] = dt * X(x[i][j] + k1x[i][j] / 2, y[i][j] + k1y[i][j] / 2, DX, DY);
+            k2y[i][j] = dt * Y(x[i][j] + k1x[i][j] / 2, y[i][j] + k1y[i][j] / 2, DX, DY);
+            k2vx[i][j] = dt * X(vx[i][j] + k1vx[i][j] / 2, vy[i][j] + k1vy[i][j] / 2, DX, DY);
+            k2vy[i][j] = dt * Y(vx[i][j] + k1vx[i][j] / 2, vy[i][j] + k1vy[i][j] / 2, DX, DY);
         }
     }
 
     #pragma omp parallel for
     for (int i = 0; i < WINDOW_WIDTH; i++) {
         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-            k3x[i][j] = dt * X(x[i][j] + k2x[i][j] / 2, y[i][j] + k2y[i][j] / 2);
-            k3y[i][j] = dt * Y(x[i][j] + k2x[i][j] / 2, y[i][j] + k2y[i][j] / 2);
-            k3vx[i][j] = dt * X(vx[i][j] + k2vx[i][j] / 2, vy[i][j] + k2vy[i][j] / 2);
-            k3vy[i][j] = dt * Y(vx[i][j] + k2vx[i][j] / 2, vy[i][j] + k2vy[i][j] / 2);
+            k3x[i][j] = dt * X(x[i][j] + k2x[i][j] / 2, y[i][j] + k2y[i][j] / 2, DX, DY);
+            k3y[i][j] = dt * Y(x[i][j] + k2x[i][j] / 2, y[i][j] + k2y[i][j] / 2, DX, DY);
+            k3vx[i][j] = dt * X(vx[i][j] + k2vx[i][j] / 2, vy[i][j] + k2vy[i][j] / 2, DX, DY);
+            k3vy[i][j] = dt * Y(vx[i][j] + k2vx[i][j] / 2, vy[i][j] + k2vy[i][j] / 2, DX, DY);
         }
     }
 
     #pragma omp parallel for
     for (int i = 0; i < WINDOW_WIDTH; i++) {
         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-            k4x[i][j] = dt * X(x[i][j] + k3x[i][j], y[i][j] + k3y[i][j]);
-            k4y[i][j] = dt * Y(x[i][j] + k3x[i][j], y[i][j] + k3y[i][j]);
-            k4vx[i][j] = dt * X(vx[i][j] + k3vx[i][j], vy[i][j] + k3vy[i][j]);
-            k4vy[i][j] = dt * Y(vx[i][j] + k3vx[i][j], vy[i][j] + k3vy[i][j]);
+            k4x[i][j] = dt * X(x[i][j] + k3x[i][j], y[i][j] + k3y[i][j], DX, DY);
+            k4y[i][j] = dt * Y(x[i][j] + k3x[i][j], y[i][j] + k3y[i][j], DX, DY);
+            k4vx[i][j] = dt * X(vx[i][j] + k3vx[i][j], vy[i][j] + k3vy[i][j], DX, DY);
+            k4vy[i][j] = dt * Y(vx[i][j] + k3vx[i][j], vy[i][j] + k3vy[i][j], DX, DY);
         }
     }
 
@@ -115,6 +141,9 @@ void rk4 (double** x, double** y, double** vx, double** vy, double dt) {
             vy[i][j] += (k1vy[i][j] + 2 * k2vy[i][j] + 2 * k3vy[i][j] + k4vy[i][j]) / 6;
         }
     }
+
+    diffusion(x, y, x, y, Dd, dt, DX, DY);
+    // diffusion(vy, vx, vx, vy, Dd, dt, DX, DY);
 
     for (int i = 0; i < WINDOW_WIDTH; i++) {
         delete[] k1x[i];
@@ -154,26 +183,26 @@ void rk4 (double** x, double** y, double** vx, double** vy, double dt) {
 }
 
 
+//диффузия которая была ДО
 
+// void diffusion(double** arr, double** temp, double D, double dt, double dx, double dy) {
+//     for (int i = 0; i < WINDOW_WIDTH; ++i) {
+//         memcpy(temp[i], arr[i], WINDOW_HEIGHT * sizeof(double));
+//     }
 
-void compute_diffusion(double** arr, double** temp, double D, double dt, double dx, double dy) {
-    for (int i = 0; i < WINDOW_WIDTH; ++i) {
-        memcpy(temp[i], arr[i], WINDOW_HEIGHT * sizeof(double));
-    }
+//     double dx2 = dx * dx;
+//     double dy2 = dy * dy;
+//     double Ddt = D * dt;
 
-    double dx2 = dx * dx;
-    double dy2 = dy * dy;
-    double Ddt = D * dt;
-
-    #pragma omp parallel for
-    for (int i = 1; i < WINDOW_WIDTH - 1; i++) {
-        for (int j = 1; j < WINDOW_HEIGHT - 1; j++) {
-            double diff_x = (temp[i - 1][j] - 2 * temp[i][j] + temp[i + 1][j]) / dx2;
-            double diff_y = (temp[i][j - 1] - 2 * temp[i][j] + temp[i][j + 1]) / dy2;
-            arr[i][j] += Ddt * (diff_x + diff_y);
-        }
-    }
-}
+//     #pragma omp parallel for
+//     for (int i = 0; i < WINDOW_WIDTH; i++) {
+//         for (int j = 0; j < WINDOW_HEIGHT; j++) {
+//             double diff_x = (temp[(i > 0 ? i - 1 : WINDOW_WIDTH - 1)][j] - 2 * temp[i][j] + temp[(i < WINDOW_WIDTH - 1 ? i + 1 : 0)][j]) / dx2;
+//             double diff_y = (temp[i][(j > 0 ? j - 1 : WINDOW_HEIGHT - 1)] - 2 * temp[i][j] + temp[i][(j < WINDOW_HEIGHT - 1 ? j + 1 : 0)]) / dy2;
+//             arr[i][j] += Ddt * (diff_x + diff_y);
+//         }
+//     }
+// }
 
 int main() {
     double D = Dd;
@@ -196,7 +225,7 @@ int main() {
         vy[i] = new double[WINDOW_HEIGHT];
         temp[i] = new double[WINDOW_HEIGHT];
         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-            x[i][j] = (double)rand() / RAND_MAX; // случайное число от 0 до 1
+            x[i][j] = (double)rand() / RAND_MAX; // rand num 0 to 1
             y[i][j] = (double)rand() / RAND_MAX;
             vx[i][j] = (double)rand() / RAND_MAX;
             vy[i][j] = (double)rand() / RAND_MAX;
@@ -220,6 +249,8 @@ int main() {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        //Для графиков
         // #pragma omp parallel for
         // for (int i = 0; i < WINDOW_HEIGHT; i += 1) {
         //     for (int j = 0; j < WINDOW_WIDTH; j += 1) {
@@ -228,19 +259,11 @@ int main() {
         //     t += dt;
         //     }
         // }
-    
-
 
         #pragma omp parallel for
-        for (int i = 0; i < WINDOW_HEIGHT; i += step_x) {
-            for (int j = 0; j < WINDOW_WIDTH; j += step_y) {
+        for (int i = 0; i < WINDOW_WIDTH; i+= step_x) {
+            for (int j = 0; j < WINDOW_HEIGHT; j+= step_y) {
                 rk4(x, y, vx, vy, dt);
-                if (i % 35 == 0 && j % 25 == 0) {
-                    // compute_diffusion(x, x, D, dt, dx, dy);
-                    // compute_diffusion(y, y, D, dt, dx, dy);
-                    // compute_diffusion(vx, vx, D, dt, dx, dy);
-                    // compute_diffusion(vy, vy, D, dt, dx, dy); 
-                }
             }
         }
         #pragma omp parallel for

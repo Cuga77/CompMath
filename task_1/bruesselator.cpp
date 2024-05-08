f06927a483e415ec01a78a08cfd011850445590e
Cuga77 <bogatov2021@mail.ru>
2024-05-08 19:04:35 +0300

    Optimize diffusion calculations and fix rk4 method

--- task_1/bruesselator.cpp
+++ task_1/bruesselator.cpp
@@ -20,6 +20,9 @@ constexpr double A = 4.6;
 constexpr double B = 1.2;
 constexpr double B1 = (B + 1);
 
+constexpr double max_ss = 0.1;
+constexpr double min_ss = 0.001;
+
 constexpr double Dd = (double)(1.5E-10);
 constexpr double H = (double)(1.5E-10);
 
@@ -91,40 +94,24 @@ double Y(double x, double y) {
 // }
 //todo: Переделать
 //Дисперсия с дискретным оператором Лапласа
-// void diffusion_with_Laplace_discrete(double x[], double H, ) {
-//     double** temp_x = new double*[WINDOW_WIDTH];
-//     double** temp_y = new double*[WINDOW_WIDTH];
-//     double dx2 = dx * dx;
-//     double dy2 = dy * dy;
-//     #pragma omp parallel for
-//     for (int i = 0; i < WINDOW_WIDTH; i++) {
-//         temp_x[i] = new double[WINDOW_HEIGHT];
-//         temp_y[i] = new double[WINDOW_HEIGHT];
-//         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-//             int i_minus = (i > 0) ? i - 1 : 1;
-//             int i_plus = (i < WINDOW_WIDTH - 1) ? i + 1 : 1;
-//             int j_minus = (j > 0) ? j - 1 : 1;
-//             int j_plus = (j < WINDOW_HEIGHT - 1) ? j + 1 : 1;
-//             double laplacian_x = (x[i_minus][j] + x[i_plus][j] + x[i][j_minus] + x[i][j_plus] - 4 * x[i][j]) / dx2;
-//             double laplacian_y = (y[i_minus][j] + y[i_plus][j] + y[i][j_minus] + y[i][j_plus] - 4 * y[i][j]) / dy2;
-//             temp_x[i][j] = x[i][j] + H * D * laplacian_x;
-//             temp_y[i][j] = y[i][j] + H * D * laplacian_y;
-//         }
-//     }
-//     for (int i = 0; i < WINDOW_WIDTH; i++) {
-//         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-//             x[i][j] = temp_x[i][j];
-//             y[i][j] = temp_y[i][j];
-//         }
-//         delete[] temp_x[i];
-//         delete[] temp_y[i];
-//     }
-//     delete[] temp_x;
-//     delete[] temp_y;
-// }
+void diffusion_with_Laplace_operator(double* arr[], double* temp[], double D, double H, double dx, double dy) {
+    for (int i = 0; i < WINDOW_WIDTH; ++i) {
+        memcpy(temp[i], arr[i], WINDOW_HEIGHT * sizeof(double));
+    }
+    double dx2 = dx * dx;
+    double dy2 = dy * dy;
+    double DH = D * H;
+    #pragma omp parallel for
+    for (int i = 0; i < WINDOW_WIDTH; i++) {
+        for (int j = 0; j < WINDOW_HEIGHT; j++) {
+            double diff_x = (temp[(i > 0 ? i - 1 : WINDOW_WIDTH - 1)][j] - 2 * temp[i][j] + temp[(i < WINDOW_WIDTH - 1 ? i + 1 : 0)][j]) / dx2;
+            double diff_y = (temp[i][(j > 0 ? j - 1 : WINDOW_HEIGHT - 1)] - 2 * temp[i][j] + temp[i][(j < WINDOW_HEIGHT - 1 ? j + 1 : 0)]) / dy2;
+            arr[i][j] += DH * (diff_x + diff_y);
+        }
+    }
+}
 
 
-//// todo: RK4  не должна знать о показаниях x и y
 void rk4 (double x[], double *t, double h) {
     double k1x, k2x, k3x, k4x;
     double k1y, k2y, k3y, k4y;
@@ -151,29 +138,11 @@ void rk4 (double x[], double *t, double h) {
     *t += h;
 }
 
-//todo: Переделать
-//диффузия которая была ДО
-// void diffusion(double** arr, double** temp, double D, double H, double dx, double dy) {
-//     for (int i = 0; i < WINDOW_WIDTH; ++i) {
-//         memcpy(temp[i], arr[i], WINDOW_HEIGHT * sizeof(double));
-//     }
-//     double dx2 = dx * dx;
-//     double dy2 = dy * dy;
-//     double DH = D * H;
-//     #pragma omp parallel for
-//     for (int i = 0; i < WINDOW_WIDTH; i++) {
-//         for (int j = 0; j < WINDOW_HEIGHT; j++) {
-//             double diff_x = (temp[(i > 0 ? i - 1 : WINDOW_WIDTH - 1)][j] - 2 * temp[i][j] + temp[(i < WINDOW_WIDTH - 1 ? i + 1 : 0)][j]) / dx2;
-//             double diff_y = (temp[i][(j > 0 ? j - 1 : WINDOW_HEIGHT - 1)] - 2 * temp[i][j] + temp[i][(j < WINDOW_HEIGHT - 1 ? j + 1 : 0)]) / dy2;
-//             arr[i][j] += DH * (diff_x + diff_y);
-//         }
-//     }
-// }
-
 
 int main() {
+    double t = 0;
     double* x = new double[WINDOW_WIDTH * WINDOW_HEIGHT * 2];
-    double t = 0.0;
+    double* temp = new double[WINDOW_WIDTH * WINDOW_HEIGHT * 2];
 
     srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Bruesselator"); 
@@ -183,34 +152,44 @@ int main() {
     sf::Image image;
     image.create(WINDOW_WIDTH, WINDOW_HEIGHT);  
     
+    
     for (int i = 0; i < WINDOW_WIDTH; i++) {
         for (int j = 0; j < WINDOW_HEIGHT; j++) {
             x[(i * WINDOW_HEIGHT + j) * 2] = (double)rand() / RAND_MAX;
             x[(i * WINDOW_HEIGHT + j) * 2 + 1] = (double)rand() / RAND_MAX;
         }
     }
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        
         for (int i = 0; i < WINDOW_WIDTH; i++) {
             for (int j = 0; j < WINDOW_HEIGHT; j++) {
                 x[(i * WINDOW_HEIGHT + j) * 2] += (double)rand() / RAND_MAX - 0.5;
                 x[(i * WINDOW_HEIGHT + j) * 2 + 1] += (double)rand() / RAND_MAX - 0.5;
                 rk4(&x[(i * WINDOW_HEIGHT + j) * 2], &t, H);
-
+            }
+        }
+        
+        
+        for (int i = 0; i < WINDOW_WIDTH; i++) {
+            for (int j = 0; j < WINDOW_HEIGHT; j++) {
                 sf::Color color = sf::Color(255 * x[(i * WINDOW_HEIGHT + j) * 2], 255 * x[(i * WINDOW_HEIGHT + j) * 2 + 1], 0);
                 image.setPixel(i, j, color);
             }
         }
+
         texture.update(image);
         sprite.setTexture(texture, true);
         window.clear();
         window.draw(sprite);
         window.display();
     }
+    delete[] temp;
     delete[] x;
     return 0;
 }

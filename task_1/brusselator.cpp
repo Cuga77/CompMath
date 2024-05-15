08e20c1f340348b29195b3f011fc5111ca5ba398
Cuga77 <bogatov2021@mail.ru>
2024-05-15 15:40:28 +0300

    Optimize diffusion calculations and fix rk4 method

--- task_1/brusselator.cpp
+++ task_1/brusselator.cpp
@@ -21,7 +21,7 @@ constexpr double A = 1.3;
 constexpr double B = 3.5;
 constexpr double B1 = (B + 1);
 
-constexpr double H = 0.002;
+constexpr double H = 0.006;
 constexpr double D = 0.00045;
 
 // compile: g++ -O3 brusselator.cpp -lsfml-graphics -lsfml-window -lsfml-system -fopenmp && ./a.out
@@ -88,16 +88,16 @@ int main() {
         }
         rk4(x, &t, H, [](double *X, double *Xdot) {
             for (int i = 0; i < size; i += 2) {
-                // double dx = (X[(i + 2) % size] - 2 * X[i] + X[(i - 2 + size) % size]) / (H * H);
-                // double dy = (X[(i + WINDOW_HEIGHT) % size] - 2 * X[i] + X[(i - WINDOW_HEIGHT + size) % size]) / (H * H); 
-                // double laplacian_x = dx + dy;
+                double dx = (X[(i + 2) % size] - 2 * X[i] + X[(i - 2 + size) % size]) / (H * H);
+                double dy = (X[(i + WINDOW_HEIGHT) % size] - 2 * X[i] + X[(i - WINDOW_HEIGHT + size) % size]) / (H * H); 
+                double laplacian_x = dx + dy;
 
-                // double dx_y = (X[(i + 2) % size + 1] - 2 * X[i + 1] + X[(i - 2 + size) % size + 1]) / (H * H);
-                // double dy_y = (X[(i + WINDOW_HEIGHT) % size + 1] - 2 * X[i + 1] + X[(i - WINDOW_HEIGHT + size) % size + 1]) / (H * H); 
-                // double laplacian_y = dx_y + dy_y;
+                double dx_y = (X[(i + 2) % size + 1] - 2 * X[i + 1] + X[(i - 2 + size) % size + 1]) / (H * H);
+                double dy_y = (X[(i + WINDOW_HEIGHT) % size + 1] - 2 * X[i + 1] + X[(i - WINDOW_HEIGHT + size) % size + 1]) / (H * H); 
+                double laplacian_y = dx_y + dy_y;
                 
-                double laplacian_x = (X[(i + 2) % size] - 2 * X[i] + X[(i - 2 + size) % size] + X[(i + WINDOW_HEIGHT) % size] - 2 * X[i] + X[(i - WINDOW_HEIGHT + size) % size]) / (H * H); //laplacian = (f(x + h) - 2f(x) + f(x - h)) / h^2
-                double laplacian_y = (X[(i + 2) % size + 1] - 2 * X[i + 1] + X[(i - 2 + size) % size + 1] + X[(i + WINDOW_HEIGHT) % size + 1] - 2 * X[i + 1] + X[(i - WINDOW_HEIGHT + size) % size + 1]) / (H * H); //laplacian = (f(x + h) - 2f(x) + f(x - h)) / h^2
+                // double laplacian_x = (X[(i + 2) % size] - 2 * X[i] + X[(i - 2 + size) % size] + X[(i + WINDOW_HEIGHT) % size] - 2 * X[i] + X[(i - WINDOW_HEIGHT + size) % size]) / (H * H); //laplacian = (f(x + h) - 2f(x) + f(x - h)) / h^2
+                // double laplacian_y = (X[(i + 2) % size + 1] - 2 * X[i + 1] + X[(i - 2 + size) % size + 1] + X[(i + WINDOW_HEIGHT) % size + 1] - 2 * X[i + 1] + X[(i - WINDOW_HEIGHT + size) % size + 1]) / (H * H); //laplacian = (f(x + h) - 2f(x) + f(x - h)) / h^2
 
                 Xdot[i] = X[i] * X[i] * X[i + 1] * 0.5 + A - B1 * X[i] + D * laplacian_x;
                 Xdot[i + 1] = -X[i] * X[i] * X[i + 1] * 0.5 + B * X[i] + D * laplacian_y;

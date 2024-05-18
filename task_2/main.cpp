cb156368d785c0162c5257b1af887b1949fe4327
Cuga77 <bogatov2021@mail.ru>
2024-05-18 10:06:15 +0300

    TAYLOR_SERIES_TERMS  MAX = 11

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -3,7 +3,7 @@
 #include "math.h"
 #include <cmath>
 
-constexpr int TAYLOR_SERIES_TERMS = 10;
+constexpr int TAYLOR_SERIES_TERMS = 11;
 
 double cos_terms(double x, double n) {
     double sum = 0.0;
@@ -14,22 +14,20 @@ double cos_terms(double x, double n) {
     return sum;
 }
 
-double double_double_cos(double * delta, double x, double n) {
-    double cos_x = cos(x);
+long double double_double_cos(long double * delta, double x, double n) {
+    long double cos_x = cosl(x);
     double cos_x_approx = cos_terms(x, n);
-    double delta_cos = cos_x - cos_x_approx;
-    *delta = delta_cos;
+    *delta = cos_x - static_cast<long double>(cos_x_approx);
     return cos_x;
 }
 
 int main() {
-    double delta;
-
+    long double delta;
     double x = 1.5;
-    double cos_x = double_double_cos(&delta, x, TAYLOR_SERIES_TERMS);
+    long double cos_x = double_double_cos(&delta, x, TAYLOR_SERIES_TERMS);
     double double_cos_x = cos_terms(x, TAYLOR_SERIES_TERMS);
-    std::cout << "Cos:\t\t   " << std::setprecision(35) << cos_x
-    << "\n" << "Double double cos: " << std::setprecision(35) << double_cos_x 
-    << "\n" << "Delta:\t\t   " << std::setprecision(35)<< delta << std::endl;
+    std::cout << "Cos:\t\t   " << std::setprecision(50) << cos_x
+    << "\n" << "Double double cos: " << std::setprecision(50) << double_cos_x 
+    << "\n" << "Delta:\t\t   " << std::setprecision(50)<< delta << std::endl;
     return 0;
 }

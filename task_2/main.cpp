2c4bd888adb73b2c4f73f266cf402f73612f02dc
Cuga77 <bogatov2021@mail.ru>
2024-05-18 09:56:37 +0300

    add cos and approximation

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
+#include <iomanip>
 #include "math.h"
 #include <cmath>
 
-double sum_two(double * delta, double a, double b) {
-    double s = a + b;
-    double bs = s - b;
-    double as = s - a;
-    *delta = (b - bs) + (a - as); 
-    return s;
+constexpr int TAYLOR_SERIES_TERMS = 10;
+
+double cos_terms(double x, double n) {
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        double term = pow(-1, i) * pow(x, 2 * i) / tgamma(2 * i + 1);
+        sum += term;
+    }
+    return sum;
 }
 
-double two_mult(double * delta, double a, double b) {
-    double p = a * b;
-    *delta = fma(a, b, -p);
-    return p;
+double double_double_cos(double * delta, double x, double n) {
+    double cos_x = cos(x);
+    double cos_x_approx = cos_terms(x, n);
+    double delta_cos = cos_x - cos_x_approx;
+    *delta = delta_cos;
+    return cos_x;
 }
 
 int main() {
-    double a = 2e-16;
-    double b = 1e-16;
-    double delta, delta2;
-    double s = sum_two(&delta, a, b);
-    std::cout << "Sum: " << s << " Delta: " << delta << std::endl;
-    double p = two_mult(&delta2, a, b);
-    std::cout << "Mult: " << p << " Delta: " << delta2 << std::endl;
+    double delta;
+
+    double x = 1.5;
+    double cos_x = double_double_cos(&delta, x, TAYLOR_SERIES_TERMS);
+    double double_cos_x = cos_terms(x, TAYLOR_SERIES_TERMS);
+    std::cout << "Cos:\t\t   " << std::setprecision(35) << cos_x
+    << "\n" << "Double double cos: " << std::setprecision(35) << double_cos_x 
+    << "\n" << "Delta:\t\t   " << std::setprecision(35)<< delta << std::endl;
     return 0;
 }
